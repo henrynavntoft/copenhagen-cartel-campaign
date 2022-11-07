e48e92ee08f59fed2e02bc8bd49b4bfaf19532e0
henrynavntoft <henrylnavntoft@gmail.com>
2022-11-07 09:36:31 +0100

    Let's gooo

---
+++ index.js
@@ -0,0 +1,32 @@
+const url =
+  "https://henrymedia.dk/copenhagen-cartel-backend/wp-json/wp/v2/product?_embed";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
+
+// Just checking
+function handleProductList(data) {
+  console.log(data);
+  data.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //Selecting template and cloning
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  // Change stuff
+  clone.querySelector("h3").textContent = product.brand;
+  clone.querySelector("h2").textContent = product.title.rendered;
+  clone.querySelector("p.price").textContent = product.price;
+  clone.querySelector("p.colors").textContent = product.colors;
+  clone.querySelector("p.stock").textContent = product.stock;
+  clone.querySelector("img").src =
+    product._embedded[
+      "wp:featuredmedia"
+    ][0].media_details.sizes.medium.source_url;
+  //Selection where i want the clone
+  const parent = document.querySelector("main");
+  //Appending it
+  parent.appendChild(clone);
+}
