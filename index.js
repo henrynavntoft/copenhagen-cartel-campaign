5e1d5d4ccc62c883f9185d7ca27acac98208494c
Monika Teiserskyte <moni6740@stud.kea.dk>
2022-11-18 16:35:03 +0100

    edited HTML, now displays colors, price, stock

--- index.js
+++ index.js
@@ -15,18 +15,29 @@ function showProduct(product) {
   //Selecting template and cloning
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
+  const colors = product._embedded["wp:term"][0];
+
   // Change stuff
-  clone.querySelector("h3").textContent = product.brand;
+  // clone.querySelector("h3").textContent = product.brand;
   clone.querySelector("h2").textContent = product.title.rendered;
-  clone.querySelector("p.price").textContent = product.price;
-  clone.querySelector("p.colors").textContent = product.colors;
-  clone.querySelector("p.stock").textContent = product.stock;
+  //displays price
+  clone.querySelector(".price span").textContent = product.price;
+  // displays colors
+  colors.forEach((color) => {
+    const liEl = document.createElement("li");
+    liEl.style.backgroundColor = color.slug;
+    console.log("testing", liEl);
+    clone.querySelector(".colour ul").appendChild(liEl);
+  });
+  // displays stock
+  clone.querySelector(".inStock span").textContent = product.stock;
+  //display image
   clone.querySelector("img").src =
     product._embedded[
       "wp:featuredmedia"
-    ][0].media_details.sizes.medium_large.source_url;
+    ][0].media_details.sizes.medium.source_url;
   //Selection where i want the clone
-  const parent = document.querySelector("main .product-feed");
+  const parent = document.querySelector("main");
   //Appending it
   parent.appendChild(clone);
 }
